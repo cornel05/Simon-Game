a62f596cfe9ada51de48f25737371816b1f1ff43
cornel05 <ngotrandinhduy2019@gmail.com>
2024-11-21 10:20:01 +0700

    boost the touch on smaller screens

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Function to change h1 text based on screen size
+// Function to update the title based on screen size
 function updateTitleForSmallScreens() {
   if (window.outerWidth <= 768) { // Tablets and phones
     $("h1").text("Tap anywhere to start");
@@ -7,18 +7,19 @@ function updateTitleForSmallScreens() {
   }
 }
 
+// Call the function on load and resize
 $(document).ready(updateTitleForSmallScreens);
-$(document).resize(updateTitleForSmallScreens);
+$(window).on("resize", updateTitleForSmallScreens);
 
+// Game variables
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
-
 var started = false;
 var level = 0;
 
-$(document).on("touchstart", function() {
+// Start game on touchstart or keypress
+$(document).on("touchstart keypress", function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -26,52 +27,42 @@ $(document).on("touchstart", function() {
   }
 });
 
-$(document).on("keypress", function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
-
-
-$(".btn").click(function() {
-
+// Button click or touch handler
+$(".btn").on("touchend click", function(event) {
+  event.preventDefault(); // Prevent default behavior
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+// Check user answer
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    // If user sequence matches game pattern
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(nextSequence, 1000);
     }
+  } else {
+    // If user sequence is wrong
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(() => $("body").removeClass("game-over"), 200);
+    startOver();
+  }
 }
 
-
+// Generate the next sequence
 function nextSequence() {
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -80,18 +71,19 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
+// Animate button press
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+  setTimeout(() => $("#" + currentColor).removeClass("pressed"), 100);
 }
 
+// Play sound
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
+// Restart the game
 function startOver() {
   level = 0;
   gamePattern = [];
